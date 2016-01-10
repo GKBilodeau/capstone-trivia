6c1689388e3d9a4ebbb930523fdf005cc94138a0
Greg Bilodeau <gbil3478@gmail.com>
2016-01-10 15:54:11 -0600

    pages controller index and show pages with appropriate content

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def index
+    categories = Unirest.get "https://pareshchouhan-trivia-v1.p.mashape.com/v1/getCategoryList",
+  headers:{
+    "X-Mashape-Key" => "KyrGOe2aGamshJFkL1NkgeypObIop18ONrKjsnzqhUJXqLzbg2",
+    "Accept" => "application/json"
+  }
+  @categories = categories.body
+  end
+
+  def show
+    category_id = params[:id]
+    @data = Unirest.get("https://pareshchouhan-trivia-v1.p.mashape.com/v1/getQuizQuestionsByCategory?categoryId=#{category_id}&limit=10&page=1",
+  headers:{
+    "X-Mashape-Key" => "KyrGOe2aGamshJFkL1NkgeypObIop18ONrKjsnzqhUJXqLzbg2",
+    "Accept" => "application/json"
+  }).body
+  end
+
+end
