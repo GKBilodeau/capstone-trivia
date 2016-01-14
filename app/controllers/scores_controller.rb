20a0c6783aee74312122967b4b416328bbffa2e8
Greg Bilodeau <gbil3478@gmail.com>
2016-01-13 20:24:29 -0600

    hooked up score model to record game score

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,11 +1,22 @@
 class ScoresController < ApplicationController
   def create
-    user_id = current_user.id
-     Score.create(
-      user_id: user_id,
+    correct = 0
+    k=1
+    10.times do
+      if params["#{k}"] == params["#{k}_correct_option"] 
+        correct += 1
+      end
+      k += 1
+      puts k
+    end
+    game_score = correct.to_f/10.to_f
+    puts game_score
+    puts correct
+    Score.create(
+      user_id: current_user.id,
       game_id: params[:id],
-      game_score: 0 
-    )
+      game_score: correct
+      )
      redirect_to '/'
   end
 
